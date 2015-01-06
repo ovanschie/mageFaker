5e98871ac823a1eedfc59d4894da201434b6a866
Olav van Schie <olavschie@hotmail.com>
2015-01-06 10:02:47 +0100

    Update product insert, meta, images & url key

--- app/code/community/Ovs/Magefaker/Model/Faker.php
+++ app/code/community/Ovs/Magefaker/Model/Faker.php
@@ -20,8 +20,6 @@ class Ovs_Magefaker_Model_Faker extends Mage_Core_Model_Abstract{
         $faker = @Faker\Factory::create();
         @$faker->addProvider(new \Faker\Provider\Product($faker));
 
-        var_dump($faker->productName);die;
-
         for($i = 0; $i < $count; $i++) {
 
             $product = Mage::getModel('catalog/product');
@@ -42,8 +40,8 @@ class Ovs_Magefaker_Model_Faker extends Mage_Core_Model_Abstract{
                         ->setStatus(1)//product status (1 - enabled, 2 - disabled)
                         ->setTaxClassId(1)//tax class (0 - none, 1 - default, 2 - taxable, 4 - shipping)
                         ->setVisibility(Mage_Catalog_Model_Product_Visibility::VISIBILITY_BOTH)//catalog and search visibility
-                        ->setManufacturer(28)//manufacturer id
-                        ->setColor(24)
+                        //->setManufacturer(28)//manufacturer id
+                        //->setColor(24)
                         ->setNewsFromDate(strtotime('now'))//product set as new from
                         ->setNewsToDate(strtotime("+1 week"))//product set as new to
                         ->setCountryOfManufacture('NL')//country of manufacture (2-letter country code)
@@ -54,16 +52,18 @@ class Ovs_Magefaker_Model_Faker extends Mage_Core_Model_Abstract{
                         //->setSpecialFromDate('06/1/2014')//special price from (MM-DD-YYYY)
                         //->setSpecialToDate('06/30/2014')//special price to (MM-DD-YYYY)
                         ->setMsrpEnabled(1)//enable MAP
-                        ->setMsrpDisplayActualPriceType(1)//display actual price (1 - on gesture, 2 - in cart, 3 - before order confirmation, 4 - use config)
+                        ->setMsrpDisplayActualPriceType(4)//display actual price (1 - on gesture, 2 - in cart, 3 - before order confirmation, 4 - use config)
                         ->setMsrp($price)//Manufacturer's Suggested Retail Price
 
-                        ->setMetaTitle('test meta title 2')
-                        ->setMetaKeyword('test meta keyword 2')
-                        ->setMetaDescription('test meta description 2')
+                        ->setMetaTitle($name)
+                        ->setMetaKeyword($faker->metaKeys)
+                        ->setMetaDescription($faker->metaDescription)
                         ->setDescription($faker->description)
                         ->setShortDescription($faker->shortDescription)
                         ->setMediaGallery(array('images' => array(), 'values' => array()))//media gallery initialization
                         ->addImageToMediaGallery($faker->productImage, array('image', 'thumbnail', 'small_image'), false, false)//assigning image, thumb and small image to media gallery
+                        ->addImageToMediaGallery($faker->productImage, array(), false, false)//assigning image, thumb and small image to media gallery
+                        ->addImageToMediaGallery($faker->productImage, array(), false, false)//assigning image, thumb and small image to media gallery
 
                         ->setStockData(array(
                                 'use_config_manage_stock' => 1, //'Use config settings' checkbox
@@ -74,7 +74,8 @@ class Ovs_Magefaker_Model_Faker extends Mage_Core_Model_Abstract{
                                 'qty' => 999 //qty
                             )
                         )
-                        ->setCategoryIds(array(0)); //assign product to categories
+                        ->setCategoryIds(array(0)) //assign product to categories
+                        ->setUrlKey($name . '-' . $sku);
 
                     $product->save();
 
