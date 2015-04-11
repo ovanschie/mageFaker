bcda23900a64ef4f372c10e638010c11a9c3c3ad
Olav van Schie <olav@360ecommerce.nl>
2015-04-11 18:58:41 +0200

    small change in product insert

--- app/code/community/Ovs/Magefaker/Model/Faker.php
+++ app/code/community/Ovs/Magefaker/Model/Faker.php
@@ -42,47 +42,50 @@ class Ovs_Magefaker_Model_Faker extends Mage_Core_Model_Abstract{
 
                 try {
                     $product
-                        ->setWebsiteIds(array(1))//website ID the product is assigned to, as an array
-                        ->setAttributeSetId(4)//ID of a attribute set named 'default'
-                        ->setTypeId('simple')//product type
-                        ->setCreatedAt(strtotime('now'))//product creation time
-                        ->setSku($sku)//SKU
-                        ->setName($name)//product name
-                        ->setWeight($faker->weight)
-                        ->setStatus(1)//product status (1 - enabled, 2 - disabled)
-                        ->setTaxClassId(0)//tax class (0 - none, 1 - default, 2 - taxable, 4 - shipping)
-                        ->setVisibility(Mage_Catalog_Model_Product_Visibility::VISIBILITY_BOTH)//catalog and search visibility
-                        ->setNewsFromDate(strtotime('now'))//product set as new from
-                        ->setNewsToDate(strtotime("+1 week"))//product set as new to
-                        ->setCountryOfManufacture('NL')//country of manufacture (2-letter country code)
+                        ->setWebsiteIds(array(1))
+                        ->setAttributeSetId(4) // default set
+                        ->setTypeId('simple')
+                        ->setCreatedAt(strtotime('now'))
 
-                        ->setPrice($price)//price in form 11.22
-                        ->setCost(($price * 0.66))//price in form 11.22
-                        ->setMsrpEnabled(1)//enable MAP
-                        ->setMsrpDisplayActualPriceType(4)//display actual price (1 - on gesture, 2 - in cart, 3 - before order confirmation, 4 - use config)
-                        ->setMsrp($price)//Manufacturer's Suggested Retail Price
+                        ->setSku($sku)
+                        ->setName($name)
+                        ->setUrlKey($name . '-' . $sku)
+                        ->setCategoryIds($categories)
+                        ->setWeight($faker->weight)
+
+                        ->setStatus(1) // product status (1 - enabled, 2 - disabled)
+                        ->setTaxClassId(0) //tax class (0 - none, 1 - default, 2 - taxable, 4 - shipping)
+                        ->setVisibility(Mage_Catalog_Model_Product_Visibility::VISIBILITY_BOTH)
+                        ->setNewsFromDate(strtotime('now'))
+                        ->setNewsToDate(strtotime("+1 week"))
+                        ->setCountryOfManufacture('NL')
+
+                        ->setPrice($price)
+                        ->setCost(($price * 0.66))
+                        ->setMsrpEnabled(1)
+                        ->setMsrpDisplayActualPriceType(4) // display actual price (1 - on gesture, 2 - in cart, 3 - before order confirmation, 4 - use config)
+                        ->setMsrp($price)
 
                         ->setMetaTitle($name)
                         ->setMetaKeyword($faker->metaKeys)
                         ->setMetaDescription($faker->metaDescription)
                         ->setDescription($faker->description)
                         ->setShortDescription($faker->shortDescription)
-                        ->setMediaGallery(array('images' => array(), 'values' => array()))//media gallery initialization
-                        ->addImageToMediaGallery($faker->productImage, array('image', 'thumbnail', 'small_image'), false, false)//assigning image, thumb and small image to media gallery
-                        ->addImageToMediaGallery($faker->productImage, array(), false, false)//assigning image, thumb and small image to media gallery
-                        ->addImageToMediaGallery($faker->productImage, array(), false, false)//assigning image, thumb and small image to media gallery
+
+                        ->setMediaGallery(array('images' => array(), 'values' => array()))
+                        ->addImageToMediaGallery($faker->productImage, array('image', 'thumbnail', 'small_image'), false, false)
+                        ->addImageToMediaGallery($faker->productImage, array(), false, false)
+                        ->addImageToMediaGallery($faker->productImage, array(), false, false)
 
                         ->setStockData(array(
-                                'use_config_manage_stock' => 1, //'Use config settings' checkbox
-                                'manage_stock' => 1, //manage stock
-                                'min_sale_qty' => 1, //Minimum Qty Allowed in Shopping Cart
-                                'max_sale_qty' => 99, //Maximum Qty Allowed in Shopping Cart
-                                'is_in_stock' => 1, //Stock Availability
-                                'qty' => 999 //qty
+                                'use_config_manage_stock' => 1,
+                                'manage_stock' => 1,
+                                'min_sale_qty' => 1,
+                                'max_sale_qty' => 99,
+                                'is_in_stock' => 1,
+                                'qty' => 999
                             )
-                        )
-                        ->setCategoryIds($categories) //assign product to categories
-                        ->setUrlKey($name . '-' . $sku);
+                        );
 
                     $product->save();
 
